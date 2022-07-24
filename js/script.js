9a08f6d3ac02989b8c2b69524a56306db6f4ceba
lemanpashayeva <lmnpasayeva@gmail.com>
2022-07-24 12:03:02 +0400

    third commit

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ for (let i = 1 ;i<=25 ;i++) {
     console.log(i);
 }
 
+//exe-2
+
+const myObj = {
+    bookname: "Harry Potter",
+    genre: "adventure",
+    price: "20$"
+};
+
+for (let x in myObj) {
+   console.log(myObj[x]);
+};
